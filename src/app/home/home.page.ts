778394dd7b3b8d567b0bad24865d7f2a27f93d3c
Frederico Holland <frederico.holland@gmail.com>
2020-12-27 16:19:03 -0300

    Implementado a primeira tela de listagem de séries, adicionado estilos e organizado a estrutura da pasta de serviços

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SeriesService } from './../services/series.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  series = [];
+
+  constructor(private seriesService: SeriesService) {}
+
+  ngOnInit() {
+    this.loadSeries();
+  }
+
+  loadSeries() {
+    this.seriesService
+      .getSeries()
+      .subscribe(res => {
+        this.series = [...this.series, ...res];
+      });
+  }
 
 }
