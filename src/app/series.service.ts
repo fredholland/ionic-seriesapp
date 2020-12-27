d7a01ca46ce07559b39cc98146ee8cb44867ac39
Frederico Holland <frederico.holland@gmail.com>
2020-12-27 15:54:52 -0300

    Implementado serviço series para conectar com a API do Episodate

--- src/app/series.service.ts
+++ src/app/series.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SeriesService {
 
-  constructor() { }
+  baseUrl = 'https://www.episodate.com/api';
+
+  constructor(private http: HttpClient) { }
+
+  getSeries() {
+    return this.http
+      .get(`${this.baseUrl}/most-popular/`)
+      .pipe(
+        map(result => {
+          return result['tv_shows'];
+        })
+      )
+  }
+
+  getSerie(id) {
+    return this.http
+      .get(`${this.baseUrl}/show-details?q=` + id)
+      .pipe(
+        map(result => {
+          return result;
+        })
+      )
+  }
+
 }
